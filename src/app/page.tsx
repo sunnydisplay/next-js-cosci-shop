fd647bf162248245806543b084d83de3cfd9e0a2
Hong <jantraratty92@gmail.com>
2025-05-27 11:35:59 +0700

    button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,21 @@
+'use client'
+
 import AppWelcome from "./component/App Welcome";
+
 
 export default function Home() {
   const title = 'Welcome sunny';
   const currentYear = <p>2025</p>
   const isShow = true;
+  const handleClick = () => {
+    alert('hi sunny');
+  } 
 
   return (
     <div>
       {/* <h1>Sunny Site</h1> */}
       <AppWelcome />
+      <button className="bg-amber-300 text-amber-50 rounded-lg" onClick={handleClick}>clicke here</button>
       <p>{title.toUpperCase()}</p>
       {currentYear}
       {
