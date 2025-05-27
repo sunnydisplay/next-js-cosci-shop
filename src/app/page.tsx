e4a3176274d169b28adee9b459076bec265b64a7
Hong <jantraratty92@gmail.com>
2025-05-27 11:18:43 +0700

    learn conditional rendering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,22 @@
 import AppWelcome from "./component/App Welcome";
 
 export default function Home() {
+  const title = 'Welcome sunny';
+  const currentYear = <p>2025</p>
+  const isShow = true;
+
   return (
     <div>
-      <h1>Sunny Site</h1>
-      <hr />
+      {/* <h1>Sunny Site</h1> */}
       <AppWelcome />
+      <p>{title.toUpperCase()}</p>
+      {currentYear}
+      {
+        isShow && <p>Date: 10/02/2000</p> 
+      }
+      {
+        isShow ? <p>Hello next.js</p> : <p>Hello Javascript</p>
+      }
     </div>
   );
 }
