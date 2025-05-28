786b111bcba772ffccf02054fd2e7fabf2d35cee
Hong <jantraratty92@gmail.com>
2025-05-28 10:55:06 +0700

    footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 'use client'
 
+import Footer04Page from "@/components/footer-04/footer-04";
 import Hero02 from "@/components/hero-02/hero-02";
 
 export default function Home() {
   return (
     <>
       <Hero02/>
+      <Footer04Page />
     </>
   );
 }
