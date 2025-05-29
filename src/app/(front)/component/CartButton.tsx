48a3bf38ee107d72ae9db57512d6d140926bf83e
Hong <jantraratty92@gmail.com>
2025-05-29 13:38:37 +0700

    basket

---
+++ src/app/(front)/component/CartButton.tsx
@@ -0,0 +1,25 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Button } from "@/components/ui/button";
+import { useCartStore } from "@/lib/cart-store";
+import { ChevronLeft, ChevronRight } from "lucide-react";
+
+export default function CartButton({ products }: any) {
+    const addItem = useCartStore((state) => state.addItem);
+
+    const handleAddItem = () => {
+        addItem({
+            productId: products.id,
+            title: products.title,
+            price: products.price,
+            qty: 1
+        });
+    }
+  return (
+    <>
+      <Button className="mt-6 shadow-none" onClick={handleAddItem}>
+            หยิบใส่ตระกร้า <ChevronRight />
+            </Button>
+    </>
+  );
+}
