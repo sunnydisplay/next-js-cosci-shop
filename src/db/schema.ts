3f89adadf9f66a15e403aca6d70df57e4bfbeaf1
Hong <jantraratty92@gmail.com>
2025-05-30 10:28:25 +0700

    user id schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -18,14 +18,14 @@ export const session = mysqlTable("session", {
  updatedAt: timestamp('updated_at').notNull(),
  ipAddress: text('ip_address'),
  userAgent: text('user_agent'),
- userId: text('user_id').notNull().references(()=> user.id, { onDelete: 'cascade' })
+ userId: varchar('user_id', { length: 36 }).notNull().references(()=> user.id, { onDelete: 'cascade' })
                 });
 
 export const account = mysqlTable("account", {
                     id: varchar('id', { length: 36 }).primaryKey(),
                     accountId: text('account_id').notNull(),
  providerId: text('provider_id').notNull(),
- userId: text('user_id').notNull().references(()=> user.id, { onDelete: 'cascade' }),
+ userId: varchar('user_id', { length:36 }).notNull().references(()=> user.id, { onDelete: 'cascade' }),
  accessToken: text('access_token'),
  refreshToken: text('refresh_token'),
  idToken: text('id_token'),
